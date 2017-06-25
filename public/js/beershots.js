7fdc6fa5b847f567ad0b9936f88916647bd41adf
gpimblott <gordon@pimblott.com>
2017-06-25 22:03:51 +0100

    Fix menubar and https link

--- public/js/beershots.js
+++ public/js/beershots.js
@@ -1,28 +1,20 @@
 
 
 function doPubSearch (query , locationiq_key ) {
-    console.log("performing search:" + query);
     if( checkPostCode(query)) {
-        console.log("It's a postcode");
-        $.getJSON("http://api.postcodes.io/postcodes/" + encodeURI(query))
-            .done(function (data) {
+        $.getJSON("https://api.postcodes.io/postcodes/" + encodeURI(query))
+            .done((data) => {
                 if( data.status===200 ) {
                     console.log("found");
                     window.location = '/pubs/' + data.result.latitude + '/' + data.result.longitude + '/';
                 }
             });
     } else {
-        console.log("Looking for a place");
         $.getJSON("https://locationiq.org/v1/search.php?key=" + locationiq_key + "&countrycodes=gb&format=json&q=" + query)
-            .done(function (data) {
-                console.log("done:")
-                console.log(data);
-                var found = data[ 0 ];
+            .done((data) => {
+                const found = data[ 0 ];
 
-                console.log(data);
-
-                if (found != undefined) {
-                    console.log("found");
+                if (found !== undefined) {
                     window.location = '/pubs/' + data[ 0 ].lat + '/' + data[ 0 ].lon + '/';
                 }
             });
